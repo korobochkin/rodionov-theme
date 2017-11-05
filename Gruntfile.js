b8ba65f39f099f14bbf7598daecb8708fd2d91ba
Kolya Korobochkin <me@korobochkin.com>
2017-11-05 18:08:27 +0300

    1

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,45 +32,6 @@ module.exports = function (grunt) {
                 dest: 'style.min.css'
             }
         }
-
-        /*autoprefixer: {
-          options: {
-            browsers: configBridge.config.autoprefixerBrowsers
-          },
-          style: {
-            options: {
-              map: true
-            },
-            src: [
-              'style.css',
-              'assets/css/admin/main-debug.css'
-            ]
-          }
-        },*/
-
-        /*cssmin: {
-          options: {
-            compatibility: 'ie8',
-            keepSpecialComments: '*',
-            sourceMap: true,
-            sourceMapInlineSources: false,
-            advanced: false
-          },
-          style: {
-            src: 'style.css',
-            dest: 'style.min.css'
-          }
-        },*/
-
-        // Copy
-        /*copy: {
-            scripts__select2: {
-                expand: true,
-                cwd: 'node_modules/select2/dist/',
-                src: ['**!/!*'],
-                dest: 'assets/js/select2/'
-            }
-        }*/
     });
 
     require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
