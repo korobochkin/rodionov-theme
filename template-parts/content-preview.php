30c5536d0c1a945c9cdb65ab13311f5131e500af
Kolya Korobochkin <me@korobochkin.com>
2017-12-09 20:55:21 +0300

    update mobile version

--- template-parts/content-preview.php
+++ template-parts/content-preview.php
@@ -10,27 +10,31 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-        <?php if ( 'post' === get_post_type() ) : ?>
-            <div class="entry-meta text-mono links-no-decoration"><?php rodionov_theme_post_meta(); ?></div>
-        <?php endif;
-			the_title( '<h2 class="entry-title text-mono links-no-decoration"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-        ?>
-	</header><!-- .entry-header -->
+    <div class="row">
+        <?php if( has_post_thumbnail() ) : ?>
+            <div class="entry-cover col-12">
+                <figure class="entry-preview">
+                    <a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
+                        <?php the_post_thumbnail(); ?>
+                    </a>
+                </figure>
+            </div>
+        <?php endif; ?>
 
-    <?php if( has_post_thumbnail() ) : ?>
-        <div class="entry-cover">
-            <figure class="entry-preview">
-                <a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
-                    <?php the_post_thumbnail(); ?>
-                </a>
-            </figure>
-        </div>
-    <?php endif; ?>
+        <header class="entry-header col-12">
+            <?php if ( 'post' === get_post_type() ) : ?>
+                <div class="entry-meta text-mono links-no-decoration"><?php rodionov_theme_post_meta(); ?></div>
+            <?php endif;
+            the_title( '<h2 class="entry-title text-mono links-no-decoration"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+            ?>
+        </header><!-- .entry-header -->
 
-    <div class="entry-excerpt row">
-        <div class="col-12 col-lg-7">
-            <?php the_excerpt(); ?>
+        <div class="entry-excerpt col-12">
+            <div class="row">
+                <div class="col-12 col-lg-7">
+                    <?php the_excerpt(); ?>
+                </div>
+            </div>
         </div>
     </div>
 </article><!-- #post-<?php the_ID(); ?> -->
